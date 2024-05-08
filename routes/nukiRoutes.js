58fd7dedb636186b8180f848565df4ae8b2b7d89
root <stefan@nikolaus-consulting.de>
2024-05-08 22:42:03 +0200

    20240508-2242

--- routes/nukiRoutes.js
+++ routes/nukiRoutes.js
@@ -7,9 +7,15 @@ var router = express.Router();
 
 router.post('/', (req, res) => {
 
-	nuki = req.body
+	callback = req.body
+	console.log(callback.batteryCritical)
+	if (callback.batteryCritical) {
+		req.body.batteryCritical = 1
+	} else {
+		callback.batteryCritical = 0
+	}
 	//console.log(req.body)
-	mqtt.sendMessage(nuki.nukiId + "/state", JSON.stringify(req.body))
+	mqtt.sendMessage(callback.nukiId + "/state", JSON.stringify(callback))
 	res.sendStatus(200);
 	//res.status(404).send({ message: "No devices found"});
     //res.status(500).send({ message: "Error retrieving devices " + err});
